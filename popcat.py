2d814378144d5f2a85bc5530b69d8271ded7c8f7
page2me kitarotao <page2me@msn.com>
2021-08-17 16:40:07 +0700

    Add files via upload

--- popcat.py
+++ popcat.py
@@ -1,21 +1,22 @@
-# import requests
-import cloudscraper
-import time
-import json
-from random import randint
-scraper = cloudscraper.create_scraper()  # returns a CloudScraper instance
+from cloudscraper import create_scraper
+from time import sleep
+from tqdm import tqdm
+from json import loads
 
+scraper = create_scraper()
 token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJDb3VudHJ5Q29kZSI6IlRIIiwiQ291bnRyeU5hbWUiOiJUaGFpbGFuZCIsIklQIjoiMTgwLjE4My4xNTMuMjMiLCJJRCI6MzAsImV4cCI6MTYyOTA0OTU0M30.ZHk25AqVJcibQQI2_FhNYgOENmFcRL-zcQ5Njd3zc-Y"
-captcha_token = "03AGdBq27u2XpAaLzTGbKTTTWbGM0ofRGfC79QEc933r5Jzml445tNYvSExkiod-6muplfyUHliz_pIUUH5jTTqffRY84N4a7i2YV2qIUs_1K7g42OwZlMzfpjrqmZ2jW8st74wTrwXewMOu1XnGC6QJ1erb8EbuX-6L1LwjiV2S4yOfX6IfaVrZO6KIsGPRfFGOzBz__hzVCL1WXei4ftwMFUnYm8ycCcVR4wZBwxuTC3jFh4hq6egtok2YHAcAZIwLk_dHK0gr1mznyCMRIwerYUMzBaNp1ZltTnP8E_0e4DSf7I29AlaKgQY-6yCtWmYwAZeGCDfI4hlpIInmj_w1atOIrCRdiSdYGL4NzgFJzXn2pMqEcYlt437cEcL2zRmzXgwhcvtTUM-QCpypRbUAg3_xRj6Prv-9C_gdX-lat0dKBN3l-nDIc"
-clicked = 0
-for i in range(100000):
-	url = f"https://stats.popcat.click/pop?pop_count=800&captcha_token={captcha_token}&token={token}"
-	res = scraper.get(url)
-	if (res.text.startswith("{") and res.text.endswith("}")) or res.status_code == 201:
-		res_json = json.loads(res.text)
-		clicked += 800
-		print(f"POP :O clicked : {clicked}")
-		token = res_json["Token"]
-	else:
-		print(f"PIP :( {res.status_code}")
-	time.sleep(30) #delay a bit
+captcha_token = "03AGdBq24ogIg7hFppU7PK7QvsspohvIOTHGObGhuR6sbnz43U1pv7uH5AogiPVuFrBAU95HIGrKLthMsFNJPb79hZmWXHoFKVqOC1xWlNjOh49gon_UFNgo55qiSqNvEi2N2Cs8cnlnnt1aG2lrLM1vxPjpO1z-q-OmUokhuS2AYViFjSN8gH_f5E6PtWgwfHAaPbvv91uBBVmr5Qlx16POMt3g9j5jMRCiwk28nSA6L969sSrpCinaSGoGTdsmyMovs-I9HdGHfpbXspOGvxs7HEope5vg-mpiKRJa-sqA03Tm206lhbugpJzUd_XobD_FRpBwTbxbQn9QChZBb0KHw-KiYrpvKq5bWCr_7lTEF-8AAmlSzQiwZrEJ5y2JTeWlvAvNQaXFGVhw0ujSTCMOO2-9jPCfbdgrNv19ocoV6i_KNM3L8R-e-qTKgYyAMrxeT2FfKmUuRY"
+HTTP201Created = 201
+pop_count = 800
+delay_time = 30
+scale = 100
+clicks = 0
+while True:
+ url = f"https://stats.popcat.click/pop?pop_count={pop_count}&captcha_token={captcha_token}&token={token}"
+ res = scraper.get(url)
+ if res.status_code == HTTP201Created:
+  res_json = loads(res.text)
+  token = res_json["Token"]
+  clicks += pop_count
+ for i in tqdm(range(scale), desc="POPCAT clicks: "+str(clicks)):
+  sleep(delay_time/scale)
--- popdog.py
+++ popdog.py
@@ -1,11 +1,16 @@
-import requests, json, time
+from requests import post
+from time import sleep
+from tqdm import tqdm
+from json import loads
 import warnings
+
 warnings.filterwarnings("ignore")
+delay_time = 2
+scale = 10
 url = 'https://www.popdog.click/clicked/v2'
-data = { "clicks": 2000,"uuid": "8745407d-0219-406d-ac09-058d2794b4dc","username": None}
-for i in range(100000):
-	res = requests.post(url, json = data,verify=False)
-	if res.status_code == 200:
-		res_data = json.loads(res.text)
-		print(f"clicks : {res_data['clicks']}")
-		time.sleep(3)
+data = {"clicks" : 2000, "uuid" : "10a63aa0-190a-4dba-bcbd-77024f32c89c", "username" : "page2me"}
+while True:
+ res = post(url, json=data, verify=False)
+ res_json = loads(res.text)
+ for i in tqdm(range(scale),desc="POPDOG clicks: "+str(res_json["clicks"])):
+  sleep(delay_time/scale)
